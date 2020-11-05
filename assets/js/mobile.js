31ee6e79d2c25ae8ee35c1ea6cb4d137830f60f5
makramloughman <54935805+makramloughman@users.noreply.github.com>
2020-11-05 18:44:15 +0200

    update

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,10 +1,15 @@
-if (screen.width <= 1200) {
-  setTimeout(redirect, 20000);
-  var mobile = document.querySelector('.mobile');
-  var par = mobile.querySelector('.p');
-  var j = 20;
-  for (var i = 0; i < 21; i++) {
-    setTimeout(function(){par.innerHTML = `The device you are using to play Space Invaders is not compatible. Please access the website from a PC. <br> <br> You will be redirected to the GitHub page of the project in ${j--} seconds.`}, i*1000);
+window.addEventListener("load", showPage);
+
+function showPage() {
+  if (screen.width <= 1200) {
+    var mobile = document.querySelector('.mobile');
+    mobile.classList.add('activem');
+    setTimeout(redirect, 20000);
+    var par = mobile.querySelector('.p');
+    var j = 20;
+    for (var i = 0; i < 21; i++) {
+      setTimeout(function(){par.innerHTML = `The device you are using to play Space Invaders is not compatible. Please access the website from a PC. <br> <br> You will be redirected to the GitHub page of the project in ${j--} seconds.`}, i*1000);
+    }
   }
 }
 
